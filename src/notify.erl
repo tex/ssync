55fa1c6542ce83abb9618c7678d7539b4c32b817
Milan Svoboda <milan.svoboda@centrum.cz>
2012-12-02 21:53:33 +0100

    allow to configure notification output
    ssync.app.src -> {env, [{output, ['notify-send', console]}]}

--- src/notify.erl
+++ src/notify.erl
@@ -7,7 +7,24 @@
 -define(TIME_PER_LINE, 3000).
 
 notify(Title, Msg) ->
+    {ok, Outputs} = application:get_env(ssync, output),
+    lists:foreach(
+        fun(Output) ->
+                notify(Output, Title, Msg)
+        end, Outputs ).
+
+notify('notify-send', Title, Msg) ->
     Msg1 = re:replace(Msg, "'", "\\\\'", [global, {return, list}]),
     Msg2 = re:replace(Msg1, "\\n", "\\\\n", [global, {return, list}]),
     Timeout = case length(Msg) of 0 -> 100; Lines -> ?TIME_PER_LINE * Lines end,
-    os:cmd(io_lib:format("notify-send -t ~p \"~s\" \"~s\"", [Timeout, Title, Msg2])).
+    os:cmd(io_lib:format("notify-send -t ~p \"~s\" \"~s\"", [Timeout, Title, Msg2]));
+
+notify(console, Title, []) ->
+    io:format("~s~n", [Title]);
+
+notify(console, Title, Msg) ->
+    Msgs = string:tokens(Msg, "\\n"),
+    lists:foreach(
+        fun(X) ->
+                io:format("~s: ~s~n", [Title, X])
+        end, Msgs ).
