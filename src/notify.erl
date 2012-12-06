3563a1cbd1c18c6bec31d4b60b67612c814021a0
Slava Yurin <v.yurin@office.ngs.ru>
2012-12-06 16:05:23 +0700

    Console: prints each line separately with title

--- src/notify.erl
+++ src/notify.erl
@@ -6,25 +6,22 @@
 
 -define(TIME_PER_LINE, 3000).
 
-notify(Title, Msg) ->
+notify(Title, Msgs) ->
     {ok, Outputs} = application:get_env(ssync, output),
     lists:foreach(
         fun(Output) ->
-                notify(Output, Title, Msg)
+                notify(Output, Title, Msgs)
         end, Outputs ).
 
-notify('notify-send', Title, Msg) ->
-    Msg1 = re:replace(Msg, "'", "\\\\'", [global, {return, list}]),
+notify('notify-send', Title, Msgs) ->
+    Msg1 = re:replace(Msgs, "'", "\\\\'", [global, {return, list}]),
     Msg2 = re:replace(Msg1, "\\n", "\\\\n", [global, {return, list}]),
-    Timeout = case length(Msg) of 0 -> 100; Lines -> ?TIME_PER_LINE * Lines end,
+    Timeout = case length(Msgs) of 0 -> 100; Lines -> ?TIME_PER_LINE * Lines end,
     os:cmd(io_lib:format("notify-send -t ~p \"~s\" \"~s\"", [Timeout, Title, Msg2]));
 
 notify(console, Title, []) ->
     io:format("~s~n", [Title]);
-
-notify(console, Title, Msg) ->
-    Msgs = string:tokens(Msg, "\\n"),
-    lists:foreach(
-        fun(X) ->
-                io:format("~s: ~s~n", [Title, X])
-        end, Msgs ).
+notify(console, Title, Msgs) ->
+    lists:foreach(fun(Msg) ->
+            io:format("~s: ~s~n", [Title, Msg])
+        end, Msgs).
