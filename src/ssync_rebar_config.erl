f5d1a6b20f095dcfe19f6f469826f10a96214620
Milan Svoboda <milan.svoboda@centrum.cz>
2012-12-11 08:11:10 +0100

    fix get_all_dirs/1 to properly make recursive search

--- src/ssync_rebar_config.erl
+++ src/ssync_rebar_config.erl
@@ -5,26 +5,20 @@
 get_all_dirs(Root) ->
     case file:consult(filename:join(Root, "rebar.config")) of
         {ok, Terms} ->
-            lists:foldl(
-                fun ({Path, CallbackName}, Acc) ->
-                        Acc ++ [{Path, CallbackName}];
-                    (Path, Acc) ->
-                        Acc ++ get_all_dirs(filename:join([Root, Path]))
-                end, [], get_deps(Root, Terms) ++ get_sub_dirs(Root, Terms) );
+            {ok, Dirs} = application:get_env(ssync, dirs),
+            [{filename:join([Root, Path]), CallbackName} ||
+                {Path, CallbackName} <- Dirs ] ++
+            [get_all_dirs(Dir) || Dir <- get_deps(Root, Terms)] ++
+            [get_all_dirs(filename:join([Root, Dir])) || Dir <- get_sub_dirs(Root, Terms)];
         {error, _} ->
-            get_sub_dirs(Root, [])
+            {ok, Dirs} = application:get_env(ssync, dirs),
+            [{filename:join([Root, Path]), CallbackName} ||
+                {Path, CallbackName} <- Dirs ]
     end.
 
 get_deps(Root, Terms) ->
     DepsDir = proplists:get_value(deps_dir, Terms, "deps"),
-    {ok, Dirs} = application:get_env(ssync, dirs),
-    [{filename:join([Dir, Path]), CallbackName} ||
-        Dir <- filelib:wildcard(filename:join([Root, DepsDir, "*"])),
-        {Path, CallbackName} <- Dirs ].
+    filelib:wildcard(filename:join([Root, DepsDir, "*"])).
 
-get_sub_dirs(Root, Terms) ->
-    SubDirs = proplists:get_value(sub_dirs, Terms, [""]),
-    {ok, Dirs} = application:get_env(ssync, dirs),
-    [{filename:join([Root, Dir, Path]), CallbackName} ||
-        Dir <- SubDirs,
-        {Path, CallbackName} <- Dirs ].
+get_sub_dirs(_Root, Terms) ->
+    proplists:get_value(sub_dirs, Terms, []).
