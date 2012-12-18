3d13ac1d73b0bfb0d7a7164497a90db2c9d1f3ac
Milan Svoboda <milan.svoboda@centrum.cz>
2012-12-18 17:39:45 +0100

    fix output to notify-send
    handle newlines correctly

--- src/ssync_notify.erl
+++ src/ssync_notify.erl
@@ -34,14 +34,19 @@ notify(Title, Msgs) ->
         end, Outputs ).
 
 notify('notify-send', Title, Msgs) ->
-    Msg1 = re:replace(Msgs, "'", "\\\\'", [global, {return, list}]),
-    Msg2 = re:replace(Msg1, "\\n", "\\\\n", [global, {return, list}]),
+    Msg1 = lists:foldl(
+            fun(Msg, Acc) ->
+                    Acc ++ io_lib:format("~s\\\\n", [Msg])
+            end, [], Msgs ),
+    Msg2 = re:replace(Msg1, "\"", "\\\\\"", [global, {return, list}]),
     Timeout = case length(Msgs) of 0 -> 100; Lines -> ?TIME_PER_LINE * Lines end,
     os:cmd(io_lib:format("notify-send -t ~p \"~s\" \"~s\"", [Timeout, Title, Msg2]));
 
 notify(console, Title, []) ->
     io:format("~s~n", [Title]);
+
 notify(console, Title, Msgs) ->
-    lists:foreach(fun(Msg) ->
+    lists:foreach(
+        fun(Msg) ->
             io:format("~s: ~s~n", [Title, Msg])
-        end, Msgs).
+        end, Msgs ).
